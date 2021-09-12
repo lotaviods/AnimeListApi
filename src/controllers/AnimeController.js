c47f6479d0b654e731becdd678977827f63adb32
Luiz Otávio <luiz.otavio@ftdata.com.br>
2021-09-12 03:21:40 -0300

    fix: AnimeConstructor

--- src/controllers/AnimeController.js
+++ src/controllers/AnimeController.js
@@ -3,13 +3,14 @@ const anime = require('../model/anime');
 const mongoose = require("../database");
 const moment = require('moment-timezone');
 const router = express.Router()
-router.post('/anime', async (req, res) => {
+const animeConstructor = require("../helper/AnimeConstructor")
+router.post('/anime', async(req, res) => {
     try {
         const Anime = mongoose.model("anime");
 
         const anime = new Anime();
 
-        let newAnime = animeConstructor({
+        let newAnime = animeConstructor.animeConstructor({
             anime: anime,
             description: anime.description,
             name: req.body.name
@@ -17,18 +18,18 @@ router.post('/anime', async (req, res) => {
 
         newAnime.save().then(anime => {
             console.info("New anime saved:", anime),
-                res.status(201).send({"anime": anime.name})
+                res.status(201).send({ "anime": anime.name })
         }, error => {
             console.info("Error")
             res.status(500).send()
         })
     } catch (e) {
         res.status(500)
-        res.send({error: "Internal server error"})
+        res.send({ error: "Internal server error" })
         console.error(e)
     }
 })
-router.get('/anime', async (req, res) => {
+router.get('/anime', async(req, res) => {
     console.info("GET: ", req.socket.remoteAddress, "Make request at: ", moment.tz(Date.now(), process.env.TIME))
     const Anime = mongoose.model("anime");
     let animeList = [];
